653ebb1c02911a1129e8d1f94e048b658d3c8a9c
Greg Svoboda <gregsvo@gmail.com>
2019-06-24 13:54:24 -0400

    Fix for new sonoff device with no gpio 14

--- thermo.py
+++ thermo.py
@@ -1,26 +1,23 @@
 import time, machine, onewire, ds18x20
-from machine import Pin, Signal
+from machine import Pin, Signal, Timer
 
 
 CONFIG = {
-    "THERMO_SENSOR_PIN": 14,
+    "THERMO_SENSOR_PIN": 3,
     "RELAY_PIN": 12,
     "LOW": 34,
-    "HIGH": 40
+    "HIGH": 40,
+    "LED_PIN": 13
 }
 
 relay_pin = CONFIG.get("RELAY_PIN")
 relay = Signal(Pin(relay_pin, Pin.OUT), invert=False)
+led = Signal(Pin(CONFIG.get('LED_PIN'), Pin.Out), invert=False)
 
 def main():
-    setup_pins()
     while True:
         time.sleep_ms(8000)
         thermo()
-
-
-def setup_pins():
-
 
 # create the ds18x20/onewire object
 def setup_DS():
@@ -63,3 +60,4 @@ def thermo():
         air_temp = fetch_ds_data(ds_object, roms_list)
         refridge_too_warm = should_turn_on_relay(air_temp)
         relay.on() if refridge_too_warm else relay.off()
+        led.on() if refridge_too_warm else led.off()
